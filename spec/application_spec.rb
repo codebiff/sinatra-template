d022d5daecd5e55e937bba3da0fb2e3171f98d7e
Code Biff <me@codebiff.com>
2012-03-16 06:58:29 +0000

    added capybara support to testsuite

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,10 +1,14 @@
-describe "Application" do
+describe "Application", :type => :request do
 
   describe "GET /" do
     
     it "should fine the index page" do
       get "/"
       last_response.should be_ok
+    end
+
+    it "should work with Capybara" do
+      visit "/"
     end
 
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "rspec"
 require "rack/test"
+require "capybara/rspec"
 
 ENV["RACK_ENV"] = "test"
 
@@ -10,3 +11,5 @@ end
 def app
   Rack::Builder.parse_file(File.dirname(__FILE__) + '/../config.ru').first
 end
+
+Capybara.app = app
