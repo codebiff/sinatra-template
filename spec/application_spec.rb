06f4b80c1e80ac5d8f3b2dcb309492cbbb3b9f80
Code Biff <me@codebiff.com>
2012-03-17 22:17:00 +0000

    dunno

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -2,13 +2,14 @@ describe "Application", :type => :request do
 
   describe "GET /" do
     
-    it "should fine the index page" do
+    it "should find the index page" do
       get "/"
       last_response.should be_ok
     end
 
     it "should work with Capybara" do
       visit "/"
+      page.should have_content "The index page"
     end
 
   end
