32a9bd27fa7b8562f495d41b0d69cbc192fddec7
Code Biff <me@codebiff.com>
2012-03-19 18:14:25 +0000

    more spec

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,10 +1,20 @@
 describe "Application", :type => :request do
 
   describe "GET /" do
-    
-    it "should find the index page" do
+
+    it "should be able to load the index page" do
       visit "/"
+      page.status_code.should == 200
       page.should have_content "The index page"
+    end
+
+  end
+  
+  describe "GET /unknown" do
+    
+    it "should return a 404 on an unknown route" do
+      visit "/unknown_route" 
+      page.status_code.should == 404
     end
 
   end
